5b9e39e398fe842f63f90e66341c977551de1910
ErikHedaker <erik@hedaker.se>
2024-12-27 12:22:27 +0100

    update

--- automated_setup.ps1
+++ automated_setup.ps1
@@ -1,9 +1,12 @@
 #winget --upgrade all
-#Python
+#Python (conda? pip?)
 #VSCode
 winget install --id Git.Git --accept-package-agreements
 $gitPath = Join-Path $env:USERPROFILE "\AppData\Local\Programs\Git\bin"
 [System.Environment]::SetEnvironmentVariable("PATH", ("{0};{1}" -f $env:PATH, $gitPath), "User")
 $env:PATH = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+#git remote add origin https://github.com/ErikHedaker/Scripts.git
+#git pull 
 #git setup code
 #git fetch scripts
+#https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
